96ca390ef6a0451a193eb335e324988593f8f728
pawelczak <lukaspawelczak@gmail.com>
2017-10-29 11:48:54 +0100

    Directive native services

--- ngx-view/test/unit/di/di.spec.ts
+++ ngx-view/test/unit/di/di.spec.ts
@@ -1,14 +1,19 @@
-import { ApplicationRef, ChangeDetectorRef, Component, ElementRef, Injector, ViewContainerRef } from '@angular/core';
+import {
+	ApplicationRef, ChangeDetectorRef, Component, Directive, ElementRef, Injector, TemplateRef, ViewChild, ViewContainerRef
+} from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 
 describe('Dependency Injection -', () => {
 
-
-	describe('component -', () => {
+	describe('Component -', () => {
 
 		@Component({
-			selector: 'di',
-			template: ``
+			selector: 'di-component',
+			template: `
+
+				<ng-template></ng-template>
+			`
 		})
 		class InjectComponent {
 
@@ -33,16 +38,77 @@ describe('Dependency Injection -', () => {
 
 		it ('should have all native services injected', () => {
 
-			// when
+			// given
 			const fixture = TestBed.createComponent(InjectComponent),
 				compInstance = fixture.componentInstance;
 
-			// then
+			// when & then
 			expect(compInstance.applicationRef).toBeDefined();
 			expect(compInstance.elementRef).toBeDefined();
 			expect(compInstance.viewContainerRef).toBeDefined();
 			expect(compInstance.injector).toBeDefined();
 			expect(compInstance.changeDetectionRef).toBeDefined();
+		});
+
+	});
+
+	describe('Directive -', () => {
+
+		@Directive({
+			selector: '[di-directive]',
+			exportAs: 'dirRef'
+		})
+		class InjectDirective {
+
+			constructor(
+				public applicationRef: ApplicationRef,
+				public elementRef: ElementRef,
+				public viewContainerRef: ViewContainerRef,
+				public injector: Injector,
+				public changeDetectionRef: ChangeDetectorRef) {}
+
+			methodOnDirective() {}
+		}
+
+		@Component({
+			selector: 'test-di-directive',
+			template: `
+				<div di-directive #dirRef="dirRef" >
+				</div>
+			`
+		})
+		class TestInjectDirectiveComponent {
+			@ViewChild('dirRef')
+			dirRef: InjectDirective;
+		}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					imports: [],
+					declarations: [
+						InjectDirective,
+						TestInjectDirectiveComponent
+					]
+				});
+		});
+
+		it ('should have all native services injected', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestInjectDirectiveComponent),
+				testCompInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(testCompInstance.dirRef.applicationRef).toBeDefined();
+			expect(testCompInstance.dirRef.elementRef).toBeDefined();
+			expect(testCompInstance.dirRef.viewContainerRef).toBeDefined();
+			expect(testCompInstance.dirRef.injector).toBeDefined();
+			expect(testCompInstance.dirRef.changeDetectionRef).toBeDefined();
+			expect(testCompInstance.dirRef.methodOnDirective).toBeDefined();
 		});
 
 	});
