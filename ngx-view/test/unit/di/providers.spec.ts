0c1af6019dca79917b60fa7d7775f57755a65214
pawelczak <lukaspawelczak@gmail.com>
2017-11-08 23:52:17 +0100

    Dependency injection - useFactory with deps & multi providers

--- ngx-view/test/unit/di/providers.spec.ts
+++ ngx-view/test/unit/di/providers.spec.ts
@@ -21,7 +21,7 @@ describe('Dependency injection - providers -', () => {
 			// then
 			expect(injector.get(Service)).toBeDefined();
 			expect(injector.get(Service) instanceof Service).toBe(true);
-		})
+		});
 	});
 
 	describe('useValue -', () => {
@@ -40,7 +40,7 @@ describe('Dependency injection - providers -', () => {
 
 			// then
 			expect(injector.get(Service)).toBe(service);
-		})
+		});
 	});
 
 	describe('useExisting -', () => {
@@ -62,7 +62,7 @@ describe('Dependency injection - providers -', () => {
 			// then
 			expect(injector.get(serviceInjectionToken)).toBeDefined();
 			expect(injector.get(serviceInjectionToken) instanceof Service).toBe(true);
-		})
+		});
 	});
 
 	describe('useFactory -', () => {
@@ -84,7 +84,67 @@ describe('Dependency injection - providers -', () => {
 			// then
 			expect(injector.get(Service)).toBeDefined();
 			expect(injector.get(Service) instanceof Service).toBe(true);
-		})
+		});
+
+		it ('should create service with dependencies', () => {
+
+			class ServiceWithDeps {
+
+				constructor(public service: Service) {}
+			}
+
+			//given
+			const providers = [
+				Service,
+				{
+					provide: ServiceWithDeps,
+					useFactory: (service: Service) => {
+						return new ServiceWithDeps(service);
+					},
+					deps: [
+						Service
+					]
+				}];
+
+			// when
+			const injector = ReflectiveInjector.resolveAndCreate(providers);
+
+			// then
+			expect(injector.get(ServiceWithDeps)).toBeDefined();
+			expect(injector.get(ServiceWithDeps) instanceof ServiceWithDeps).toBe(true);
+			expect(injector.get(ServiceWithDeps).service).toBeDefined();
+			expect(injector.get(ServiceWithDeps).service instanceof Service).toBe(true);
+		});
+
+		describe('multi -', () => {
+
+			it ('should create array of services', () => {
+
+				//given
+				const token = new InjectionToken('token');
+				const providerValues = ['providerOne', 'providerTwo', 'providerThree'];
+				const providers = [{
+					provide: token,
+					useValue: providerValues[0],
+					multi: true
+				}, {
+					provide: token,
+					useValue: providerValues[1],
+					multi: true
+				}, {
+					provide: token,
+					useValue: providerValues[2],
+					multi: true
+				}];
+
+				// when
+				const injector = ReflectiveInjector.resolveAndCreate(providers);
+
+				// then
+				expect(injector.get(token)).toEqual(providerValues);
+			});
+
+		});
 	});
 
 });
