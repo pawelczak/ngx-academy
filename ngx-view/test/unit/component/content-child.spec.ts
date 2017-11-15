7d9aa49a44b2026a76ac61aaa82426c49ba27f6e
pawelczak <lukaspawelczak@gmail.com>
2017-11-16 00:01:30 +0100

    ContentChild - ng-template references

--- ngx-view/test/unit/component/content-child.spec.ts
+++ ngx-view/test/unit/component/content-child.spec.ts
@@ -1,27 +1,48 @@
-import { Component, ContentChild, ViewChild } from '@angular/core';
+import { Component, ContentChild, TemplateRef, ViewChild } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 
 describe('ContentChild -', () => {
 
-	describe('cotent references -', () => {
+	@Component({
+		selector: 'simple',
+		template: ``
+	})
+	class SimpleComponent {}
 
-		@Component({
-			selector: 'simple',
-			template: ``
-		})
-		class SimpleComponent {}
+	@Component({
+		selector: 'content-child',
+		template: ``
+	})
+	class ContentChildComponent {
 
-		@Component({
-			selector: 'content-child',
-			template: ``
-		})
-		class ContentChildComponent {
+		/**
+		 * component references
+		 */
+		@ContentChild(SimpleComponent)
+		simpleComponent: SimpleComponent;
 
-			@ContentChild(SimpleComponent)
-			simpleComponent: SimpleComponent;
+		/**
+		 * ng-template references
+		 */
+		@ContentChild(TemplateRef)
+		templateRef: TemplateRef<any>;
 
-		}
+		/**
+		 * template variables references
+		 */
+		@ContentChild('templateOne')
+		templateOneVarRef: TemplateRef<any>;
+
+		@ContentChild('templateTwo')
+		templateTwoVarRef: TemplateRef<any>;
+
+		@ContentChild('templateThree')
+		templateThreeVarRef: TemplateRef<any>;
+
+	}
+
+	describe('component references -', () => {
 
 		@Component({
 			selector: 'root',
@@ -60,6 +81,51 @@ describe('ContentChild -', () => {
 			// then
 			expect(compInstance.contentChildRef.simpleComponent).toBeDefined();
 			expect(compInstance.contentChildRef.simpleComponent instanceof SimpleComponent).toBeDefined();
+		});
+
+	});
+
+	describe('ng-template references -', () => {
+
+		@Component({
+			selector: 'root',
+			template: `
+				<content-child>
+					<ng-template #templateOne ></ng-template>
+					<ng-template #templateTwo ></ng-template>
+					<ng-template #templateThree ></ng-template>
+				</content-child>
+			`
+		})
+		class RootComponent {
+			@ViewChild(ContentChildComponent)
+			contentChildRef: ContentChildComponent;
+		}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					imports: [],
+					declarations: [
+						RootComponent,
+						ContentChildComponent,
+						SimpleComponent
+					]
+				});
+		});
+
+		it ('should be possible to get reference to component', () => {
+
+			// given
+			const fixture = TestBed.createComponent(RootComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(compInstance.contentChildRef.templateRef).toBeDefined();
+			expect(compInstance.contentChildRef.templateRef instanceof TemplateRef).toBeDefined();
 		});
 
 	});
