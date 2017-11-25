b9c32b64c72b7d1045a8073cb49897074702a37b
pawelczak <lukaspawelczak@gmail.com>
2017-11-25 19:25:20 +0100

    Http - params, errors

--- ngx-view/test/unit/http/http.spec.ts
+++ ngx-view/test/unit/http/http.spec.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 
-describe('Dependency injection - providers -', () => {
+describe('HttpClient -', () => {
 
 	class Car {
 		constructor(private type: string) {}
@@ -19,14 +19,16 @@ describe('Dependency injection - providers -', () => {
 
 		constructor(private httpClient: HttpClient) {}
 
-		getCars() {
-			return this.httpClient.get<Array<Car>>('cars');
+		getCars(url = 'cars', params?: HttpParams) {
+			return this.httpClient.get<Array<Car>>(url, { params });
 		}
 
 	}
 
 	let carsService: CarsService,
 		httpMock: HttpTestingController;
+
+	const cars = [new Car('combi'), new Car('suv')];
 
 	beforeEach(() => {
 		TestBed
@@ -49,20 +51,74 @@ describe('Dependency injection - providers -', () => {
 	});
 
 
-	it ('mock simple http calls', () => {
-
-		// given
-		const cars = [new Car('combi'), new Car('suv')];
+	it ('mock simple http calls', (done) => {
 
 		// when & then
 		carsService.getCars().subscribe((requestedCars) => {
 			expect(requestedCars).toEqual(cars);
 			expect(requestedCars[0] instanceof Car).toBeTruthy();
 			expect(requestedCars[0].getType()).toBe(cars[0].getType());
+			done();
 		});
 
 		httpMock.expectOne('cars')
 			.flush(cars);
+	});
+
+	it ('should return type of request and params', (done) => {
+
+		// given
+		const url = 'cars?max=100';
+
+		// when & then
+		carsService.getCars(url).subscribe((requestedCars) => {
+			expect(requestedCars).toEqual(cars);
+			done();
+		});
+
+		const request = httpMock.expectOne(url);
+		expect(request.request.url).toBe(url);
+		expect(request.request.params).toEqual(new HttpParams());
+
+		request.flush(cars);
+	});
+
+	it ('should handle errors', (done) => {
+
+		// given
+		const url = 'cars';
+
+		// when & then
+		carsService
+			.getCars(url)
+			.subscribe(
+				(next) => {},
+				(error) => {
+					done();
+				}
+			);
+
+		const request = httpMock.expectOne(url);
+		request.error(new ErrorEvent(null, null));
+	});
+
+	it ('should return Http params', (done) => {
+
+		// given
+		const url = 'cars';
+		const params = new HttpParams().set('max', '200');
+
+		// when & then
+		carsService.getCars(url, params).subscribe((requestedCars) => {
+			expect(requestedCars).toEqual(cars);
+			done();
+		});
+
+		const request = httpMock.expectOne(url + '?max=200');
+		expect(request.request.url).toBe(url);
+		expect(request.request.params).toEqual(params);
+
+		request.flush(cars);
 	});
 
 });
