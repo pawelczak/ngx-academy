48eee582f57f0e44ead6f61ea32e77098569c953
pawelczak <lukaspawelczak@gmail.com>
2017-12-25 00:47:13 +0100

    ContentChildren - descendants

--- ngx-view/test/unit/component/content-children.spec.ts
+++ ngx-view/test/unit/component/content-children.spec.ts
@@ -149,6 +149,94 @@ describe('ContentChildren -', () => {
 			expect(simpleCompRefs.length).toEqual(1);
 			expect(simpleCompRefs[0].value).toEqual('#1');
 		});
-	})
+	});
+
+	describe ('descendants -', () => {
+
+		@Component({
+			selector: 'content-children-descendants',
+			template: ``
+		})
+		class ContentChildrenDescendantsComponent {
+
+			/**
+			 * component references with descendants false
+			 */
+			@ContentChildren(SimpleComponent, {descendants: false})
+			simpleComponent: QueryList<SimpleComponent>;
+
+			/**
+			 * component references with descendants true
+			 */
+			@ContentChildren(SimpleComponent, {descendants: true})
+			simpleComponentWithDescendants: QueryList<SimpleComponent>;
+		}
+
+		@Component({
+			selector: 'test',
+			template: `
+
+				<content-children-descendants>
+
+					<simple [value]="'#1'" >
+					</simple>
+
+					<simple [value]="'#2'" >
+						<simple [value]="'#3'" >#3</simple>
+					</simple>
+
+				</content-children-descendants>
+
+			`
+		})
+		class TestComponent {
+			@ViewChild(ContentChildrenDescendantsComponent)
+			compRef: ContentChildrenDescendantsComponent;
+		}
+
+		beforeEach(() => {
+			TestBed.configureTestingModule({
+				declarations: [
+					SimpleComponent,
+					ContentChildrenDescendantsComponent,
+					TestComponent
+				]
+			});
+		});
+
+		it ('should get content from first level - no descendants', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			let simpleCompRefs = compInstance.compRef.simpleComponent.toArray();
+			expect(simpleCompRefs.length).toEqual(2);
+			expect(simpleCompRefs[0].value).toEqual('#1');
+			expect(simpleCompRefs[1].value).toEqual('#2');
+		});
+
+		it ('should get all content children - with descendants', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			let simpleComponentWithDescendants = compInstance.compRef.simpleComponentWithDescendants.toArray();
+			expect(simpleComponentWithDescendants.length).toEqual(3);
+			expect(simpleComponentWithDescendants[0].value).toEqual('#1');
+			expect(simpleComponentWithDescendants[1].value).toEqual('#2');
+			expect(simpleComponentWithDescendants[2].value).toEqual('#3');
+		});
+
+	});
 
 });
