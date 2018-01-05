23e0795c1e8aa37c299266ccfee833bf7bfaa4e4
pawelczak <lukaspawelczak@gmail.com>
2018-01-05 08:21:51 +0100

    Attribute - component

---
+++ ngx-view/test/unit/di/attribute.spec.ts
@@ -0,0 +1,75 @@
+import { Attribute, Component, ViewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+/**
+ * @Attribute allows to get value from a component or directive
+ */
+describe('@Attribute -', () => {
+
+	/**
+	 * Taken values are converted to strings
+	 */
+	describe('component -', () => {
+
+		const givenId = 28,
+			givenCssClass = 'nice-button',
+			givenConfig = '{id: 8, name: \'test\'}';
+
+		@Component({
+			selector: 'attr-test',
+			template: ``
+		})
+		class AttrComponent {
+
+			constructor(@Attribute('id') public id: any,
+						@Attribute('class') public cssClass: any,
+						@Attribute('data-config') public config: any) {}
+		}
+
+		@Component({
+			selector: 'test',
+			template: `
+				<attr-test
+					id="${givenId}"
+					class="${givenCssClass}"
+					data-config="${givenConfig}" ></attr-test>
+			`
+		})
+		class TestComponent {
+			@ViewChild(AttrComponent)
+			attrRef: AttrComponent;
+		}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					declarations: [
+						TestComponent,
+						AttrComponent
+					]
+				});
+		});
+
+		it ('should get attribute from a component', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance.attrRef;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(compInstance.id).toEqual('' + givenId);
+			expect(typeof compInstance.id).toEqual('string');
+
+			expect(compInstance.cssClass).toEqual(givenCssClass);
+			expect(typeof compInstance.cssClass).toEqual('string');
+
+			expect(compInstance.config).toEqual(givenConfig);
+			expect(typeof compInstance.config).toEqual('string');
+		});
+
+	});
+
+});
