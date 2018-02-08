baf35b6c3d5347efca5ad237ef39c7d0f0e948f8
pawelczak <lukaspawelczak@gmail.com>
2018-02-08 12:00:13 +0100

    NgFor - trackBy - mutation objects

--- ngx-view/test/unit/directive/native/ng-for.spec.ts
+++ ngx-view/test/unit/directive/native/ng-for.spec.ts
@@ -1,6 +1,9 @@
-import { Component, Injectable } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Injectable, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
+import { ComponentFixture } from '@angular/core/testing/src/component_fixture';
+import { By } from '@angular/platform-browser';
 
 
 describe('NgFor -', () => {
@@ -86,6 +89,9 @@ describe('NgFor -', () => {
 
 	});
 
+	/**
+	 * Changing trackBy makes only sense to use it with ChangeDetectionStrategy.OnPush
+	 */
 	describe('trackBy -', () => {
 
 		let logger: Logger;
@@ -103,7 +109,8 @@ describe('NgFor -', () => {
 
 		@Component({
 			selector: 'simple',
-			template: ``
+			template: `<ng-content></ng-content>`,
+			changeDetection: ChangeDetectionStrategy.OnPush
 		})
 		class SimpleComponent {
 			constructor(private logger: Logger) {}
@@ -118,8 +125,11 @@ describe('NgFor -', () => {
 			@Component({
 				selector: 'test',
 				template: `
-					<simple *ngFor="let hero of heroes"></simple>
-				`
+					<simple *ngFor="let hero of heroes">
+						{{hero}}
+					</simple>
+				`,
+				changeDetection: ChangeDetectionStrategy.OnPush
 			})
 			class TestComponent {
 
@@ -169,12 +179,12 @@ describe('NgFor -', () => {
 				expect(logger.logs.length).toBe(compInstance.heroes.length);
 			});
 
-			it('should render only new items added to array', () => {
+			xit('should render only new items added to array', () => {
 
 				// given
 				const fixture = TestBed.createComponent(TestComponent),
 					compInstance = fixture.componentInstance,
-					newHero = 'Voldemorth';
+					newHero = 'Gandalf';
 
 				fixture.detectChanges();
 
@@ -187,6 +197,205 @@ describe('NgFor -', () => {
 				// one additional render
 				expect(logger.logs.length).toBe(compInstance.heroes.length);
 			});
+
+		});
+
+		describe('object -', () => {
+
+			class Hero {
+				constructor(public id: number,
+							public name: string) {}
+
+				clone(): Hero {
+					return new Hero(this.id, this.name);
+				}
+			}
+
+			@Component({
+				selector: 'hero',
+				template: `{{hero.name}}`,
+				changeDetection: ChangeDetectionStrategy.OnPush
+			})
+			class HeroComponent {
+				@Input()
+				hero: Hero;
+
+				constructor(private logger: Logger) {}
+
+				ngAfterViewInit() {
+					this.logger.log('Hero component render');
+				}
+			}
+
+			@Component({
+				selector: 'test',
+				template: `
+					<hero *ngFor="let hero of heroes" [hero]="hero">
+					</hero>
+				`,
+				changeDetection: ChangeDetectionStrategy.OnPush
+			})
+			class TestComponent {
+
+				heroOne = new Hero(1, 'spiderman');
+				heroTwo = new Hero(2,'wolverine');
+				heroThree = new Hero(3,'xavier');
+
+				heroes = [this.heroOne, this.heroTwo, this.heroThree];
+
+				constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+				uniqueHero(index: number, hero: Hero) {
+					return hero.id;
+				}
+
+				rebuild() {
+					this.changeDetectorRef.detectChanges();
+				}
+			}
+
+			beforeEach(() => {
+				TestBed
+					.configureTestingModule({
+						imports: [
+							CommonModule
+						],
+						declarations: [
+							HeroComponent,
+							TestComponent
+						],
+						providers: [
+							Logger
+						]
+					});
+			});
+
+			/**
+			 * Reference to objects inside array hasn't changed
+			 */
+			it('should render elements only one', () => {
+
+				// given
+				logger = TestBed.get(Logger);
+				const fixture = TestBed.createComponent(TestComponent),
+					compInstance = fixture.componentInstance;
+
+				fixture.detectChanges();
+
+				expect(logger.logs.length).toBe(compInstance.heroes.length);
+
+				// Creating new array with same the same objects
+				fixture.componentInstance.heroes = [...fixture.componentInstance.heroes];
+				compInstance.rebuild();
+				fixture.detectChanges();
+
+				// no additional renders
+				expect(logger.logs.length).toBe(compInstance.heroes.length);
+				assertHeroes(fixture);
+			});
+
+			/**
+			 * Object Mutation
+			 */
+			it('should not re-render elements when they mutate', () => {
+
+				// given
+				logger = TestBed.get(Logger);
+				const fixture = TestBed.createComponent(TestComponent),
+					compInstance = fixture.componentInstance;
+
+				fixture.detectChanges();
+
+				// when
+				// Hero mutation
+				compInstance.heroOne.id = 8;
+				compInstance.heroOne.name = 'Batman';
+				fixture.componentInstance.heroes = [...fixture.componentInstance.heroes];
+				compInstance.rebuild();
+
+				fixture.detectChanges();
+
+				// then
+				const elements = fixture.debugElement.queryAll(By.css('hero'));
+
+				expect(elements[0].nativeElement.textContent.trim()).not.toBe(compInstance.heroOne.name);
+				expect(elements[0].nativeElement.textContent.trim()).toBe('spiderman');
+				expect(logger.logs.length).toBe(compInstance.heroes.length);
+			});
+
+			/**
+			 * Changing reference to objects should make them be re-rendered.
+			 * In this case re-renders are not required, because objects are cloned,
+			 * so they are replaced with the exact copy of itself.
+			 */
+			it('should not re-render elements when they mutate', () => {
+
+				// given
+				logger = TestBed.get(Logger);
+				const fixture = TestBed.createComponent(TestComponent),
+					compInstance = fixture.componentInstance;
+
+				fixture.detectChanges();
+
+				// when
+				// Hero mutation
+				fixture.componentInstance.heroes = [
+					compInstance.heroOne.clone(),
+					compInstance.heroTwo.clone(),
+					compInstance.heroThree.clone()
+				];
+				compInstance.rebuild();
+
+				fixture.detectChanges();
+
+
+				// then
+				assertHeroes(fixture);
+				// every element is re-rendered
+				expect(logger.logs.length).toBe(compInstance.heroes.length + 3);
+			});
+
+			/**
+			 * Object Mutation + trackBy
+			 *
+			 */
+			it('should render elements when they mutate', () => {
+
+				// given
+				TestBed.overrideTemplate(TestComponent, `
+					<hero *ngFor="let hero of heroes; trackBy: uniqueHero" [hero]="hero" >
+					</hero>
+				`);
+				logger = TestBed.get(Logger);
+
+				const fixture = TestBed.createComponent(TestComponent),
+					compInstance = fixture.componentInstance;
+
+				fixture.detectChanges();
+
+				// when
+				// Hero mutation
+				fixture.componentInstance.heroes = [
+					compInstance.heroOne.clone(),
+					compInstance.heroTwo.clone(),
+					compInstance.heroThree.clone()
+				];
+				compInstance.rebuild();
+				fixture.detectChanges();
+
+				// then
+				expect(logger.logs.length).toBe(compInstance.heroes.length);
+				assertHeroes(fixture);
+			});
+
+			function assertHeroes(fixture: ComponentFixture<TestComponent>) {
+				const compInstance = fixture.componentInstance,
+					elements = fixture.debugElement.queryAll(By.css('hero'));
+
+				expect(elements[0].nativeElement.textContent.trim()).toBe(compInstance.heroOne.name);
+				expect(elements[1].nativeElement.textContent.trim()).toBe(compInstance.heroTwo.name);
+				expect(elements[2].nativeElement.textContent.trim()).toBe(compInstance.heroThree.name);
+			}
 
 		});
 
