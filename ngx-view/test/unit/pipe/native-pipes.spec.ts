f4bd45bac9cfd008c01a1fd09d95054b33d975c1
pawelczak <lukaspawelczak@gmail.com>
2018-02-11 06:58:57 +0100

    Pipes - native object pipe

--- ngx-view/test/unit/pipe/native-pipes.spec.ts
+++ ngx-view/test/unit/pipe/native-pipes.spec.ts
@@ -12,12 +12,22 @@ describe('Natives pipes -', () => {
 	 * - uppercase
 	 * - titlecase
 	 *
+	 * Date:
+	 * - date
+	 *
 	 * Object:
 	 * - json
 	 *
+	 * Number:
+	 * - number
+	 * - percent
+	 * - currency
+	 *
+	 * Array & string:
+	 * - slice
+	 *
 	 */
 	describe ('string -', () => {
-
 
 		@Component({
 			selector: 'pipes-test',
@@ -61,6 +71,70 @@ describe('Natives pipes -', () => {
 			expect(elements[1].textContent).toBe('HAN SOLO');
 			expect(elements[2].textContent).toBe('Han Solo');
 		});
+
+	});
+
+	describe('Date -', () => {
+
+
+	});
+
+	describe('Object -', () => {
+
+		@Component({
+			selector: 'pipes-test',
+			template: `
+				<p>{{object | json}}</p>
+			`
+		})
+		class PipesTestComponent {
+
+			object = {
+				jaba: 0,
+				the: 1,
+				hut: 2
+			};
+		}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					imports: [],
+					declarations: [
+						PipesTestComponent
+					]
+				});
+		});
+
+		it ('should modify template value by specific pipe', () => {
+
+			// given
+			const fixture = TestBed.createComponent(PipesTestComponent),
+				expectedValue = JSON.stringify(fixture.componentInstance.object, null, 2);
+			/**
+			 * expectedValue
+			 * '{
+				  "jaba": 0,
+				  "the": 1,
+				  "hut": 2
+				}'
+			 */
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			const elements = fixture.nativeElement.querySelectorAll('p');
+
+			expect(elements[0].textContent).toBe(expectedValue);
+		});
+	});
+
+	describe('Number -', () => {
+
+	});
+
+	describe('Array & string -', () => {
 
 	});
 
