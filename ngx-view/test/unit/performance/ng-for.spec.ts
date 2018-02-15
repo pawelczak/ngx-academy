b21ac33780f66982bc5a8f3a26100070fb7ddff3
pawelczak <lukaspawelczak@gmail.com>
2018-02-15 07:55:24 +0100

    NgFor - trackBy

--- ngx-view/test/unit/performance/ng-for.spec.ts
+++ ngx-view/test/unit/performance/ng-for.spec.ts
@@ -85,7 +85,8 @@ describe('Performance - ngFor -', () => {
 			// when
 			fixture.detectChanges();
 
-			compInstance.heroes = [changedHero, givenHeroes[1], givenHeroes[2]];
+			compInstance.heroes[0] = changedHero;
+			compInstance.heroes = [...compInstance.heroes];
 
 			fixture.detectChanges();
 
@@ -117,7 +118,6 @@ describe('Performance - ngFor -', () => {
 
 			constructor() {
 				HeroComponent.number += 1;
-				// console.log('HeroComp', HeroComponent.number);
 			}
 		}
 
@@ -128,11 +128,15 @@ describe('Performance - ngFor -', () => {
 				</hero>
 			`
 		})
-		class TestComponent {
+		class PureNgForTestComponent {
 			@ViewChildren(HeroComponent)
 			heroCompRefs: QueryList<HeroComponent>;
 
 			heroes = _.cloneDeep(givenHeroes);
+
+			trackHeroBy(index: number, hero: Hero) {
+				return hero.id;
+			}
 		}
 
 		beforeEach(() => {
@@ -142,7 +146,7 @@ describe('Performance - ngFor -', () => {
 						CommonModule
 					],
 					declarations: [
-						TestComponent,
+						PureNgForTestComponent,
 						HeroComponent
 					]
 				});
@@ -151,11 +155,15 @@ describe('Performance - ngFor -', () => {
 		it('should not re-render when object mutates', () => {
 
 			// given
-			const fixture = TestBed.createComponent(TestComponent),
+			const fixture = TestBed.createComponent(PureNgForTestComponent),
 				compInstance = fixture.componentInstance;
 
 			// when
 			fixture.detectChanges();
+
+			compInstance.heroCompRefs.toArray().forEach((heroComp) => {
+				heroComp.pure = false;
+			});
 
 			compInstance.heroes[0].name = 'Batman';
 			compInstance.heroes = [...compInstance.heroes];
@@ -165,15 +173,20 @@ describe('Performance - ngFor -', () => {
 			// then
 			const elements = fixture.debugElement.queryAll(By.css('hero'));
 
-			expect(elements[0].nativeElement.textContent.trim()).toBe(givenHeroes[0].name); // should not change
+			expect(elements[0].nativeElement.textContent.trim()).not.toBe('Batman');
+			expect(elements[0].nativeElement.textContent.trim()).toBe(givenHeroes[0].name);
 			expect(elements[1].nativeElement.textContent.trim()).toBe(givenHeroes[1].name);
 			expect(elements[2].nativeElement.textContent.trim()).toBe(givenHeroes[2].name);
+
+			compInstance.heroCompRefs.toArray().forEach((heroComp, index) => {
+				expect(heroComp.pure).toBe(false);
+			});
 		});
 
 		it('should render when object changes reference', () => {
 
 			// given
-			const fixture = TestBed.createComponent(TestComponent),
+			const fixture = TestBed.createComponent(PureNgForTestComponent),
 				compInstance = fixture.componentInstance,
 				newHero = new Hero(4, 'Batman');
 
@@ -184,7 +197,8 @@ describe('Performance - ngFor -', () => {
 				heroComp.pure = false;
 			});
 
-			compInstance.heroes = [newHero, givenHeroes[1], givenHeroes[2]];
+			compInstance.heroes[0] = newHero;
+			compInstance.heroes = [...compInstance.heroes];
 
 			fixture.detectChanges();
 
@@ -195,15 +209,17 @@ describe('Performance - ngFor -', () => {
 			expect(elements[1].nativeElement.textContent.trim()).toBe(givenHeroes[1].name);
 			expect(elements[2].nativeElement.textContent.trim()).toBe(givenHeroes[2].name);
 
-			compInstance.heroCompRefs.toArray().forEach((heroComp, index) => {
-				expect(heroComp.pure).toBe(true);
-			});
+			const heroRefs = compInstance.heroCompRefs.toArray();
+
+			expect(heroRefs[0].pure).toBe(true, 'Component [0] has been recreated');
+			expect(heroRefs[1].pure).toBe(false, 'Component [1] hasn\'t been recreated');
+			expect(heroRefs[2].pure).toBe(false, 'Component [2] hasn\'t been recreated');
 		});
 
 		it('should re-render when reference changed', () => {
 
 			// given
-			const fixture = TestBed.createComponent(TestComponent),
+			const fixture = TestBed.createComponent(PureNgForTestComponent),
 				compInstance = fixture.componentInstance,
 				newHeroes = [
 					new Hero(1, 'wolverine'),
@@ -231,6 +247,65 @@ describe('Performance - ngFor -', () => {
 
 			compInstance.heroCompRefs.toArray().forEach((heroComp) => {
 				expect(heroComp.pure).toBe(true);
+			});
+
+		});
+
+		/**
+		 * TrackBy changes behavior of ngFor.
+		 * Normally when reference changes to an object in an array,
+		 * ngFor destroys component and recreates it. When you use
+		 * trackBy, you can tell angular when to recreate component
+		 * and when not to.
+		 */
+		describe('trackBy -', () => {
+
+			const trackByTemplate = `
+				<hero *ngFor="let hero of heroes;trackBy: trackHeroBy"
+					  [hero]="hero" >
+				</hero>
+			`;
+
+			/**
+			 * In example below array of elements in changed, but new objects
+			 * has same id as old ones. That makes angular not to recreate the components,
+			 * but it changes input value of components, so new names will be rerendered properly.
+			 */
+			it('should not re-render when reference changed', () => {
+
+				// given
+				TestBed.overrideTemplate(PureNgForTestComponent, trackByTemplate);
+
+				const fixture = TestBed.createComponent(PureNgForTestComponent),
+					compInstance = fixture.componentInstance,
+					newHeroes = [
+						new Hero(1, 'Batman'),
+						new Hero(2, 'Antman'),
+						new Hero(3, 'Iceman')
+					];
+
+				// when
+				fixture.detectChanges();
+
+				compInstance.heroCompRefs.toArray().forEach((heroComp) => {
+					heroComp.pure = false;
+				});
+
+				compInstance.heroes = newHeroes;
+
+				fixture.detectChanges();
+
+				// then
+				const elements = fixture.debugElement.queryAll(By.css('hero'));
+
+				expect(elements[0].nativeElement.textContent.trim()).toBe(newHeroes[0].name);
+				expect(elements[1].nativeElement.textContent.trim()).toBe(newHeroes[1].name);
+				expect(elements[2].nativeElement.textContent.trim()).toBe(newHeroes[2].name);
+
+				compInstance.heroCompRefs.toArray().forEach((heroComp) => {
+					expect(heroComp.pure).toBe(false);
+				});
+
 			});
 
 		});
