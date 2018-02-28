38d97dee1d176cf66afb75fce40a9652e4f0a99a
pawelczak <lukaspawelczak@gmail.com>
2018-02-28 08:44:41 +0100

    Component - output - aliasing

---
+++ ngx-view/test/unit/component/output.spec.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+
+describe('Component - output -', () => {
+
+	/**
+	 * It's possible to alias outputs.
+	 */
+	describe ('naming -', () => {
+
+		const compValue = 'Value';
+
+		@Component({
+			selector: 'output-alias',
+			template: ``
+		})
+		class OutputAliasComponent {
+
+			@Output('output-one')
+			outputOne = new EventEmitter();
+
+			emit() {
+				this.outputOne.emit(compValue);
+			}
+		}
+
+		@Component({
+			selector: 'test',
+			template: `
+					<output-alias #compRef (output-one)="onEmit($event)" ></output-alias>
+				`
+		})
+		class TestComponent {
+			@ViewChild('compRef')
+			compRef: OutputAliasComponent;
+
+			value: string;
+
+			onEmit(value: any) {
+				this.value = value;
+			}
+		}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					declarations: [
+						OutputAliasComponent,
+						TestComponent
+					]
+				});
+		});
+
+		it ('should be possible to alias outputs', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+			compInstance.compRef.emit();
+
+			// then
+			expect(compInstance.value).toEqual(compValue);
+		});
+	});
+});
