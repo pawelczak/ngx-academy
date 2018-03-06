126a72a6a71a5f85f0524ed536344cefbd44076a
pawelczak <lukaspawelczak@gmail.com>
2018-03-06 07:28:03 +0100

    DI - injectionToken - useClass, useExisting

--- ngx-view/test/unit/di/injection-token.spec.ts
+++ ngx-view/test/unit/di/injection-token.spec.ts
@@ -2,26 +2,29 @@ import { Component, Inject, InjectionToken } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 /**
- * InjectionToken should be used when someone wants to add class/value
- * to the DI context.
+ * InjectionToken should be used:
+ *
+ * "Whenever the type you are injecting is not reified (does not have a
+ * runtime representation)"
+ *
  */
 describe('InjectionToken -', () => {
+
+	const tokenName = 'token',
+		token = new InjectionToken(tokenName),
+		value = 'Dean Winchester';
+
+	@Component({
+		template: ``
+	})
+	class TestComponent {
+		constructor(@Inject(token) public value: any) {}
+	}
 
 	/**
 	 * InjectionToken should be used mainly with useValue
 	 */
 	describe('provider - useValue -', () => {
-
-		const tokenName = 'token',
-			token = new InjectionToken(tokenName),
-			value = 'Dean Winchester';
-
-		@Component({
-			template: ``
-		})
-		class TestComponent {
-			constructor(@Inject(token) public value: any) {}
-		}
 
 		beforeEach(() => {
 			TestBed
@@ -47,6 +50,75 @@ describe('InjectionToken -', () => {
 			expect(fixture.componentInstance.value).toBe(value);
 		});
 
+	});
+
+	/**
+	 * InjectionToken with useClass
+	 */
+	describe('provider - useClass -', () => {
+
+		class TestClass {}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					declarations: [
+						TestComponent
+					],
+					providers: [
+						{provide: token, useClass: TestClass}
+					]
+				});
+		});
+
+		it('should be possible to use injection token with useClass', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(compInstance.value).toBeDefined();
+			expect(compInstance.value instanceof TestClass).toBeTruthy();
+		});
+	});
+
+	/**
+	 * InjectionToken with useExisting
+	 */
+	describe('provider - useExisting -', () => {
+
+		class TestClass {}
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					declarations: [
+						TestComponent
+					],
+					providers: [
+						TestClass,
+						{provide: token, useExisting: TestClass}
+					]
+				});
+		});
+
+		it('should be possible to use injection token with useExisting', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent),
+				compInstance = fixture.componentInstance;
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(compInstance.value).toBeDefined();
+			expect(compInstance.value instanceof TestClass).toBeTruthy();
+		});
 	});
 
 });
