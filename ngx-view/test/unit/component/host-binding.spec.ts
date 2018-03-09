ddf701a87833cb32a4aa00a71e398dac8cb9772f
pawelczak <lukaspawelczak@gmail.com>
2018-03-09 09:17:13 +0100

    HostBinding - different stuff

--- ngx-view/test/unit/component/host-binding.spec.ts
+++ ngx-view/test/unit/component/host-binding.spec.ts
@@ -2,7 +2,37 @@ import { Component, HostBinding } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 
+/**
+ * HostBinding / "host meta" allows to assign values to host element.
+ * The types of "things", that can be assigned:
+ * - class (takes string, assign any class)
+ * - class.className (takes boolean, assign specific class)
+ * - style.styleName (takes string value, assign value to styleName)
+ * - attr.attributeName (takes string value, assign value to attribute)
+ * - property (e.g. [id]='randomId')
+ */
 describe('HostBinding -', () => {
+
+	const color = 'purple',
+		className = 'Dean',
+		attrKey = 'name',
+		attrValue = 'Winchester',
+		propertyKey = 'id',
+		propertyValue = 'Colt';
+
+	function assertHostBindings(nativeElement: any) {
+
+		const actualClassList = nativeElement.classList,
+			actualStyleColor = nativeElement.style.color,
+			actualAttrKey = nativeElement.getAttribute(attrKey),
+			actualIdValue = nativeElement.getAttribute(propertyKey);
+
+		expect(actualClassList).toContain('bound');
+		expect(actualClassList).toContain(className);
+		expect(actualStyleColor).toEqual(color);
+		expect(actualAttrKey).toEqual(attrValue);
+		expect(actualIdValue).toEqual(propertyValue);
+	}
 
 	describe('components host property -', () => {
 
@@ -10,12 +40,24 @@ describe('HostBinding -', () => {
 			selector: '<host>',
 			template: ``,
 			host: {
-				'[class.bound]': 'isBound'
+				'[class]': 'className',
+				'[class.bound]': 'isBound',
+				'[style.color]': 'styleColor',
+				'[attr.name]': 'attrValue',
+				'[id]': 'propertyValue' // <-- property binding
 			}
 		})
 		class HostBindingComponent {
 
 			isBound = true;
+
+			styleColor = color;
+
+			className = className;
+
+			attrValue = attrValue;
+
+			propertyValue = propertyValue;
 		}
 
 		beforeEach(() => {
@@ -35,11 +77,8 @@ describe('HostBinding -', () => {
 			fixture.detectChanges();
 
 			// then
-			const classList = fixture.debugElement.nativeElement.classList;
-
-			expect(classList).toContain('bound');
+			assertHostBindings(fixture.nativeElement);
 		});
-
 	});
 
 	describe('@HostBinding() -', () => {
@@ -50,8 +89,20 @@ describe('HostBinding -', () => {
 		})
 		class HostBindingComponent {
 
+			@HostBinding('class')
+			className = className;
+
 			@HostBinding('class.bound')
 			isBound = true;
+
+			@HostBinding('style.color')
+			styleColor = color;
+
+			@HostBinding('attr.name')
+			attrValue = attrValue;
+
+			@HostBinding(propertyKey)
+			property = propertyValue;
 		}
 
 		beforeEach(() => {
@@ -71,9 +122,7 @@ describe('HostBinding -', () => {
 			fixture.detectChanges();
 
 			// then
-			const classList = fixture.debugElement.nativeElement.classList;
-
-			expect(classList).toContain('bound');
+			assertHostBindings(fixture.nativeElement);
 		});
 
 	});
