477107dfdb3b105e2ba5e02d6d47fe122db290df
pawelczak <lukaspawelczak@gmail.com>
2018-04-02 19:02:42 +0200

    Dependency Injection - Injector - getAll by toString

--- ngx-view/test/unit/di/static-injector.spec.ts
+++ ngx-view/test/unit/di/static-injector.spec.ts
@@ -3,6 +3,8 @@ import { Injector, StaticProvider } from '@angular/core';
 
 describe('StaticInjector -', () => {
 
+	class Record {}
+
 	/**
 	 *
 	 * static create(options: {
@@ -13,7 +15,7 @@ describe('StaticInjector -', () => {
 	 */
 	describe('create -', () => {
 
-		class Record {}
+
 
 		it('should be possible to create Injector with providers', () => {
 
@@ -49,6 +51,85 @@ describe('StaticInjector -', () => {
 	});
 
 
+	/**
+	 * There is no public method to get all records from Injector.
+	 * Although there should be a couple of methods how to do that.
+	 *
+	 */
+	describe('get all Records', () => {
+
+		let givenProviders: Array<StaticProvider>,
+			notFoundValue = 'Bruce Wayne';
+
+		/**
+		 * Public method toString on StaticInjector returns
+		 * all the names of the records in the injector scope.
+		 * It should be possible to get all the records by their
+		 * names.
+		 */
+		describe('toString() -', () => {
+
+			it('should be possible to get all Records by their names', () => {
+
+				// given
+				const injector = createInjectorWithRecords(),
+					foundRecords = new Map<any, any>();
+
+				// when
+				let records = injector.toString();
+				let tokens = parseRecordNames(records);
+
+				// then
+				expect(tokens.length).toBe(givenProviders.length + 1); // records + injector
+
+				tokens.forEach((token: string) => {
+
+					let record = injector.get(token, notFoundValue);
+
+					if (record !== notFoundValue) {
+						foundRecords.set(token, record);
+					}
+
+					expect(injector.get(token, notFoundValue)).toBeDefined();
+				});
+
+				expect(foundRecords.size).toBe(1);
+				expect(foundRecords.get('HERO')).toBeDefined();
+
+				/**
+				 * Conclusion
+				 * It is possible to only get records that have string tokens
+				 */
+			});
+
+			function parseRecordNames(records: string): Array<string> {
+				let splitRecords = records.split('[')[1];
+				let tokens = (splitRecords.split(']')[0]).split(', ');
+
+				return tokens;
+			}
+
+		});
+
+		function createInjectorWithRecords(): Injector {
+
+			givenProviders = [
+				{
+					provide: Record,
+					useClass: Record,
+					deps: []
+				} as StaticProvider,
+				{
+					provide: 'HERO',
+					useValue: 'Batman',
+					deps: []
+				} as StaticProvider,
+			];
+
+			return Injector.create({providers: givenProviders});
+		}
+
+	});
 
 	xit('should be possible to get all records from StaticInjector', () => {
 
