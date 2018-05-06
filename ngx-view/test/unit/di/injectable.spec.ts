a7d9428d94a759e36151aceab21f8de4f369e5c6
pawelczak <lukaspawelczak@gmail.com>
2018-05-06 19:22:28 +0200

    Injectable - providedIn

--- ngx-view/test/unit/di/injectable.spec.ts
+++ ngx-view/test/unit/di/injectable.spec.ts
@@ -1,20 +1,65 @@
+import { Injectable, NgModule } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 describe('@Injectable() -', () => {
 
+	/**
+	 * ProvidedIn allows to specify to which module
+	 * decorated sevice should be added as a provided class.
+	 */
+	describe('providedIn -', () => {
+
+		@NgModule()
+		class EmptyModule {
+		}
+
+		describe('',() => {
+
+			@Injectable({
+				providedIn: EmptyModule
+			})
+			class ProvidedInService {
+			}
+
+			beforeEach(() => {
+				TestBed
+					.configureTestingModule({
+						imports: [
+							EmptyModule
+						]
+					});
+			});
+
+			it('should allow to get services provided with use of providedIn', () => {
+
+				// given & when
+				let service = TestBed.get(ProvidedInService);
+
+				// then
+				expect(service).toBeDefined();
+				expect(service instanceof ProvidedInService).toEqual(true);
+			});
+
+		});
+
+	});
+
+
 	describe('without -', () => {
 
 		class Car {
-			constructor(public engine: Engine) {}
+			constructor(public engine: Engine) {
+			}
 		}
 
-		class Engine {}
+		class Engine {
+		}
 
 		/**
 		 * Creating a service without dependencies doesn't require the @Injectable
 		 * annotation.
 		 */
-		it ('should create service without dependencies', () => {
+		it('should create service without dependencies', () => {
 
 			TestBed
 				.configureTestingModule({
@@ -30,7 +75,7 @@ describe('@Injectable() -', () => {
 		 * When creating a service with dependencies and without Injectable annotation,
 		 * Angular should throw an error.
 		 */
-		it ('shouldn\'t be possible to create service with dependencies', () => {
+		it('shouldn\'t be possible to create service with dependencies', () => {
 
 			TestBed
 				.configureTestingModule({
