d5018af74cb2ee37288d1724e50b8fba4ef1e366
pawelczak <lukaspawelczak@gmail.com>
2018-05-15 07:56:57 +0200

    RxJS - operators - map

---
+++ rxjs-academy/test/operators/map.spec.ts
@@ -0,0 +1,33 @@
+import { cold } from 'jasmine-marbles';
+import { map } from 'rxjs/operators';
+
+
+describe('RxJS - operators -', () => {
+
+	it('should map onle value to another', () => {
+
+		// given
+		const givenValues = {
+				a: 1,
+				b: 2,
+				c: 3
+			},
+			expectedValues = {
+				x: 2,
+				y: 4,
+				z: 6
+			};
+
+		const givenValues$ = cold('-a-b-c-|', givenValues);
+		const expectedValues$ = cold('-x-y-z-|', expectedValues);
+
+		// when
+		const actualValues$ = givenValues$.pipe(
+											map(v => v * 2)
+										);
+
+		// then
+		expect(actualValues$).toBeObservable(expectedValues$);
+	});
+
+});
