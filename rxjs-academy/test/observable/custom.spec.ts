d52db03ac07fc897f2e61ae998cfddd218605ddc
pawelczak <lukaspawelczak@gmail.com>
2018-05-23 15:14:33 +0200

    Observable from subject

--- rxjs-academy/test/observable/custom.spec.ts
+++ rxjs-academy/test/observable/custom.spec.ts
@@ -1,7 +1,8 @@
 import { Observable, Operator, Observer } from 'rxjs';
 import { count, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs/Rx';
 
-describe('Observable - custom -', () => {
+describe('Observable -', () => {
 
 	const heroNames = [
 		'Wolverine',
@@ -28,58 +29,95 @@ describe('Observable - custom -', () => {
 		}
 	}
 
-	const givenHeroes = [
-			new Hero('Magento'),
-			new Hero('DeadPool'),
-			new Hero(heroNames[0]),
-			new Hero(heroNames[1])
-		],
-		expectedHeores = [...givenHeroes];
-	let actualHeroes: Array<Hero> = [],
-		heroes$: HeroObservable = new HeroObservable((observer: Observer<Hero>) => {
-			givenHeroes.forEach((h: Hero) => {
-				observer.next(h);
+	describe('custom -', () => {
+
+		const givenHeroes = [
+				new Hero('Magneto'),
+				new Hero('DeadPool'),
+				new Hero(heroNames[0]),
+				new Hero(heroNames[1])
+			],
+			expectedHeroes = [...givenHeroes];
+		let actualHeroes: Array<Hero> = [],
+			heroes$: HeroObservable = new HeroObservable((observer: Observer<Hero>) => {
+				givenHeroes.forEach((h: Hero) => {
+					observer.next(h);
+				});
+				observer.complete();
 			});
-			observer.complete();
+
+		beforeEach(() => {
+			actualHeroes = [];
 		});
 
-	beforeEach(() => {
-		actualHeroes = [];
+		it('should be possible to create own observable', (done) => {
+
+			// when
+			heroes$
+				.pipe(
+					tap((h: Hero) => actualHeroes.push(h)),
+					count()
+				)
+				.subscribe(() => {
+
+					// then
+					expect(actualHeroes).toEqual(expectedHeroes);
+					done();
+				});
+		});
+
+		it('should use custom operator', (done) => {
+
+			// when
+			heroes$
+				.isHero(...heroNames)
+				.pipe(
+					tap((h: Hero) => actualHeroes.push(h)),
+					count()
+				)
+				.subscribe(() => {
+
+					// then
+					expect(actualHeroes.length).toEqual(2);
+					expect(actualHeroes[0].name).toEqual(heroNames[0]);
+					expect(actualHeroes[1].name).toEqual(heroNames[1]);
+					done();
+				});
+		});
+
 	});
 
-	it('should be possible to create own observable', (done) => {
+	describe('Observable from other observable-', () => {
 
-		// when
-		heroes$
-			.pipe(
-				tap((h: Hero) => actualHeroes.push(h)),
-				count()
-			)
-			.subscribe(() => {
+		class UberHeroObservable<T = Hero> extends Observable<T> {
 
-				// then
-				expect(actualHeroes).toEqual(expectedHeores);
-				done();
-			});
-	});
+			constructor(source?: Observable<T>) {
+				super();
 
-	it('should use custom operator', (done) => {
+				if (source) {
+					this.source = source;
+				}
+			}
+		}
 
-		// when
-		heroes$
-			.isHero(...heroNames)
-			.pipe(
-				tap((h: Hero) => actualHeroes.push(h)),
-				count()
-			)
-			.subscribe(() => {
+		it('create observable from subject', (done) => {
 
-				// then
-				expect(actualHeroes.length).toEqual(2);
-				expect(actualHeroes[0].name).toEqual(heroNames[0]);
-				expect(actualHeroes[1].name).toEqual(heroNames[1]);
-				done();
-			});
+			const subject = new Subject(),
+				heroes$ = new UberHeroObservable(subject),
+				givenHero = new Hero('Logan');
+
+			heroes$
+				.subscribe((hero: Hero) => {
+
+					// then
+					expect(hero).toEqual(givenHero);
+					done();
+				});
+
+			// when
+			subject.next(givenHero);
+		});
+
 	});
 
 });
