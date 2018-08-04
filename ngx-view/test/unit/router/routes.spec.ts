6ebaefa54d2b3799bf1feedb6bbe004da844f046
pawelczak <lukaspawelczak@gmail.com>
2018-08-04 22:23:49 +0200

    Router - loadchildren

--- ngx-view/test/unit/router/routes.spec.ts
+++ ngx-view/test/unit/router/routes.spec.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Location } from '@angular/common';
-import { Router, Routes } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -12,88 +13,193 @@ describe('Router - routes', () => {
 	let fixture: ComponentFixture<any>;
 
 	@Component({
+		selector: 'list',
 		template: `List`
 	})
-	class ListComponent {}
+	class ListComponent {
+	}
 
 	@Component({
+		selector: 'details',
 		template: `Details`
 	})
-	class DetailsComponent {}
+	class DetailsComponent {
+	}
 
 	@Component({
 		template: `
 			<router-outlet></router-outlet>
 		`
 	})
-	class AppComponent {}
+	class AppComponent {
+	}
 
-	const routes: Routes = [
-		{path: '', redirectTo: 'list', pathMatch: 'full'},
-		{path: 'list', component: ListComponent},
-		{path: 'details', component: DetailsComponent}
-	];
 
-	beforeEach(() => {
-		TestBed
-			.configureTestingModule({
-				imports: [
-					RouterTestingModule.withRoutes(routes)
-				],
-				declarations: [
-					ListComponent,
-					DetailsComponent,
-					AppComponent
-				]
-			});
+	/**
+	 * Router direct to the specific components.
+	 */
+	describe('component path -', () => {
 
-		router = TestBed.get(Router);
-		location = TestBed.get(Location);
+		const routes: Routes = [
+			{path: '', redirectTo: 'list', pathMatch: 'full'},
+			{path: 'list', component: ListComponent},
+			{path: 'details', component: DetailsComponent}
+		];
 
-		fixture = TestBed.createComponent(AppComponent);
-		router.initialNavigation();
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					imports: [
+						RouterTestingModule.withRoutes(routes)
+					],
+					declarations: [
+						ListComponent,
+						DetailsComponent,
+						AppComponent
+					]
+				});
+
+			router = TestBed.get(Router);
+			location = TestBed.get(Location);
+
+			fixture = TestBed.createComponent(AppComponent);
+			router.initialNavigation();
+		});
+
+		/**
+		 * Testing with 'done' method
+		 */
+		it('navigate to "" redirects you to /list', (done) => {
+
+			// when
+			router
+				.navigate([''])
+				.then(() => {
+
+					// then
+					expect(location.path()).toBe('/list');
+
+					const listElement = fixture.debugElement.query(By.css('list'));
+					expect(listElement).not.toBeNull();
+					done();
+				});
+		});
+
+		/**
+		 * fakeAsync doesn't work
+		 */
+		xit('navigate to "search" takes you to /details', fakeAsync(() => {
+
+			// when
+			router.navigate(['/details']);
+
+			fixture.detectChanges();
+			tick(5000);
+
+			// then
+			expect(location.path()).toBe('/details');
+		}));
+
+		/**
+		 * (async) method works
+		 */
+		it('can navigate to details with method (async)', async(() => {
+
+			// when
+			router
+				.navigate(['/details'])
+				.then(() => {
+
+					// then
+					expect(location.path()).toBe('/details');
+
+					const detailsEl = fixture.debugElement.query(By.css('details'));
+					expect(detailsEl).not.toBeNull();
+				});
+		}));
+
 	});
 
+
 	/**
-	 * Testing with 'done' method
+	 * Router directs to the module
 	 */
-	it ('navigate to "" redirects you to /list', (done) => {
+	describe('loadChildren -', () => {
 
-		// when
-		router
-			.navigate([''])
-			.then(() => {
+		const listModuleRoutes = [
+			{path: '', component: ListComponent},
+			{path: 'details', component: DetailsComponent}
+		];
 
-				// then
-				expect(location.path()).toBe('/list');
-				done();
-			});
+		@NgModule({
+			imports: [
+				RouterModule.forChild(listModuleRoutes)
+			],
+			declarations: [
+				ListComponent,
+				DetailsComponent
+			]
+		})
+		class ChildrenModule {
+		}
+
+		const routes: Routes = [
+			{path: '', loadChildren: () => ChildrenModule}
+		];
+
+
+		beforeEach(() => {
+			TestBed
+				.configureTestingModule({
+					imports: [
+						RouterTestingModule.withRoutes(routes)
+					],
+					declarations: [
+						AppComponent
+					]
+				});
+
+			router = TestBed.get(Router);
+			location = TestBed.get(Location);
+
+			fixture = TestBed.createComponent(AppComponent);
+			router.initialNavigation();
+		});
+
+		it ('should navigate to the default path', (done) => {
+
+			// when
+			router
+				.navigate([''])
+				.then(() => {
+
+					// then
+					expect(location.path()).toBe('/');
+
+					const listElement = fixture.debugElement.query(By.css('list'));
+					expect(listElement).not.toBeNull();
+					done();
+				});
+
+		});
+
+		it ('should navigate to more specific path', (done) => {
+
+			// when
+			router
+				.navigate(['/details'])
+				.then(() => {
+
+					// then
+					expect(location.path()).toBe('/details');
+
+					const detailsElement = fixture.debugElement.query(By.css('details'));
+					expect(detailsElement).not.toBeNull();
+					done();
+				});
+
+		});
+
 	});
-
-	/**
-	 * fakeAsync doesn't work
-	 */
-	xit ('navigate to "search" takes you to /details', fakeAsync(() => {
-
-		// when
-		router.navigate(['/details']);
-
-		fixture.detectChanges();
-		tick(5000);
-
-		// then
-		expect(location.path()).toBe('/details');
-	}));
-
-	/**
-	 * (async) method works
-	 */
-	it ('can navigate to details with method (async)', async(() => {
-		router
-			.navigate(['/details'])
-			.then(() => {
-				expect(location.path()).toBe('/details');
-			});
-	}));
 
 });
