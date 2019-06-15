afd8ce281af0c3692339768a34dbcf98367f4be0
Łukasz Pawełczak <lukasz.pawelczak@softwareplant.com>
2019-06-15 11:40:37 +0200

    Component Inheritance ViewChild ContentChild

--- ngx-view/test/unit/component/inheritance/inheritance.queries.spec.ts
+++ ngx-view/test/unit/component/inheritance/inheritance.queries.spec.ts
@@ -1,5 +1,104 @@
-describe('Inheritance Queries -', () => {
+import { Component, ContentChild, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
+
+fdescribe('Inheritance Queries -', () => {
+
+	describe('ViewChild -', () => {
+
+		const givenValue = 'To the Bat-mobile !';
+
+		class ViewChildComponent {
+			@ViewChild('template')
+			template: ElementRef;
+		}
+
+		@Component({
+			template: `
+				<div #template>
+					${givenValue}
+				</div>
+			`
+		})
+		class TestComponent extends ViewChildComponent {
+		}
+
+		beforeEach(() => {
+			TestBed.configureTestingModule({
+				declarations: [
+					TestComponent
+				]
+			});
+		});
+
+		it('should create template component', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent);
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			const template = fixture.componentInstance.template;
+
+			expect(template).toBeDefined();
+			expect(template.nativeElement.textContent.trim()).toEqual(givenValue);
+
+		});
+	});
+
+	fdescribe('ContentChild -', () => {
+
+		class ContentChildComponent {
+			@ContentChild('template')
+			template: TemplateRef<any>;
+		}
+
+		@Component({
+			selector: 'content-child-inherited',
+			template: ``
+		})
+		class TestContentChildComponent extends ContentChildComponent {
+		}
+
+		@Component({
+			template: `
+				<content-child-inherited>
+					
+					<ng-template #template>
+						Because I'm Batman
+					</ng-template>
+					
+				</content-child-inherited>
+			`
+		})
+		class TestComponent {
+			@ViewChild(TestContentChildComponent)
+			contentChild: TestContentChildComponent;
+		}
+
+		beforeEach(() => {
+			TestBed.configureTestingModule({
+				declarations: [
+					TestComponent,
+					TestContentChildComponent
+				]
+			});
+		});
+
+		it('should create container component', () => {
+
+			// given
+			const fixture = TestBed.createComponent(TestComponent);
+
+			// when
+			fixture.detectChanges();
+
+			// then
+			expect(fixture.componentInstance.contentChild.template).toBeDefined();
+		});
+	});
 
 
 });
