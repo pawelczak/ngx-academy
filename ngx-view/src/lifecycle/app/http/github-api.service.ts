181eda29adaa80ecf2f9341f99f6147d61219b72
pawelczak <lukaspawelczak@gmail.com>
2020-05-09 23:02:34 +0200

    Upgrade to angular 8

--- ngx-view/src/lifecycle/app/http/github-api.service.ts
+++ ngx-view/src/lifecycle/app/http/github-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { throwError } from 'rxjs';
+import { Observable ,  throwError } from 'rxjs';
 
 export class User {
 	login: string;
